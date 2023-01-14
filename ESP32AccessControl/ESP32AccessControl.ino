8a3eb185de10c1152d55d0ed52953f0a1bcbc3cc
Bruno Castro <bruno.scastro2012@hotmail.com>
2023-01-14 02:26:56 -0300

    novo codigo de controle web

--- ESP32AccessControl/ESP32AccessControl.ino
+++ ESP32AccessControl/ESP32AccessControl.ino
@@ -1,84 +1,123 @@
-#include <WiFi.h>                                     // needed to connect to setup an accesspoint
+#include <WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include <WebSocketsServer.h>                         // needed for instant communication between client and server through Websockets
-#include <ArduinoJson.h>                              // needed for JSON encapsulation (send multiple variables with one string)
+#include <WebSocketsServer.h>
+#include <ArduinoJson.h>
 #include <SPIFFS.h>
 
-const char* ssid = "Sala1307";
-const char* password = "Senhateste";
+const char* accessPointSSID = "Sala1307";
+const char* accessPointPassword = "Senhateste";
 
-// Configure IP addresses of the local access point
 IPAddress local_IP(192,168,1,22);
 IPAddress gateway(192,168,1,5);
 IPAddress subnet(255,255,255,0);
 
-// Initialization of webserver and websocket
-AsyncWebServer server(80);                                 // the server uses port 80 (standard port for websites)
-WebSocketsServer webSocket = WebSocketsServer(81);    // the websocket uses port 81 (standard port for websockets)
- 
-void setup() {  
-  Serial.begin(115200);                               // init serial port for debugging
+AsyncWebServer webServer(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
+void initSPIFFS() {
   if (!SPIFFS.begin()) {
-    Serial.println("ERRROR - SPIFFS could not initialize");
+    Serial.println("Cannot mount SPIFFS volume... Restarting in 5 seconds!");
+    delay(5000);
+    ESP.restart();
   }
- 
+}
+
+void initAccessPoint() {
   Serial.print("Setting up Access Point ... ");
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
 
   Serial.print("Starting Access Point ... ");
-  Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
+  Serial.println(WiFi.softAP(accessPointSSID, accessPointPassword) ? "Ready" : "Failed!");
 
   Serial.print("IP address = ");
   Serial.println(WiFi.softAPIP());
+}
 
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {    // define here wat the webserver needs to do
+void initWebServer() {
+  webServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {    // define here wat the webserver needs to do
     request->send(SPIFFS, "/webpage.html", "text/html");           
   });
 
-  server.onNotFound([](AsyncWebServerRequest *request) {
-    request->send(404, "text/plain", "File not found");
+  webServer.onNotFound([](AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found. Just '/' endpoint exists!");
   });
 
-  server.serveStatic("/", SPIFFS, "/");
-  
-  webSocket.begin();                                  // start websocket
-  webSocket.onEvent(webSocketEvent);                  // define a callback function -> what does the ESP32 need to do when an event from the websocket is received? -> run function "webSocketEvent()"
+  webServer.serveStatic("/", SPIFFS, "/");
+}
 
-  server.begin();
+void initWebSocket() {
+  webSocket.begin();                                 
+  webSocket.onEvent = onWebSocketEvent;
+
+  webServer.begin();
+}
+
+void setup() {  
+  Serial.begin(115200);
+  delay(500);
+
+  initSPIFFS();
+  initAccessPoint();
+  initWebServer();
+  delay(500);
+  initWebSocket();
 }
 
 void loop() {
-//  server.handleClient();
   webSocket.loop();
 }
 
-void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {      // the parameters of this callback function are always the same -> num: id of the client who send the event, type: type of message, payload: actual data sent and length: length of payload
-  Serial.println('New Event received =' + String(type));
-  switch (type) {                                     // switch on the type of information sent
-    case WStype_DISCONNECTED:                         // if a client is disconnected, then type == WStype_DISCONNECTED
-      Serial.println("Client " + String(num) + " disconnected");
+void sendCurrentStatus() {
+  Serial.println("Send Current Status to WEB -> Não implementado");
+}
+
+void handleValidEvents(char* event_type, int event_value) {
+  switch(event_type) {
+    case "isRegistrationMode":
+      Serial.print("Set Registration Mode");
+      Serial.println(event_value == 1);
       break;
-    case WStype_CONNECTED:                            // if a client is connected, then type == WStype_CONNECTED
-      Serial.println("Client " + String(num) + " connected");
+    case "isDoorOpen":
+      Serial.print("Set Door Status");
+      Serial.println(event_value == 1);
       break;
-    case WStype_TEXT:                                 // if a client has sent data, then type == WStype_TEXT
-      // try to decipher the JSON string received
-      StaticJsonDocument<200> doc;                    // create JSON container 
-      DeserializationError error = deserializeJson(doc, payload);
-      if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.f_str());
-        return;
-      }
-      else {
-        // JSON string was received correctly, so information can be retrieved:
-        const char* l_type = doc["type"];
-        const int l_value = doc["value"];
-        Serial.println("Type: " + String(l_type));
-        Serial.println("Value: " + String(l_value));
-      }
+    case "getStatus":
+      sendCurrentStatus();
+      break;
+  };
+}
+
+void onWebSocketEvent(byte clientId, WStype_t eventType, uint8_t *payload) {
+  Serial.print("New WebSocket Event received from client with id " + String(clientId) + " -> ");
+  switch (eventType) {
+    case WStype_DISCONNECTED:
+      Serial.println("Client with ID" + String(clientId) + " disconnected now!");
+      break;
+    case WStype_CONNECTED:
+      Serial.println("Client with ID " + String(clientId) + " connected now!");
+      break;
+    case WStype_TEXT:
+      handleWebSocketTextEvent(payload);
       break;
   };
 };
+
+void handleWebSocketTextEvent(payload) {
+  StaticJsonDocument<200> doc;
+  DeserializationError error = deserializeJson(doc, payload);
+
+  if (error) {
+    Serial.print(F("Error on deserialize Json content: "));
+    Serial.println(error.f_str());
+    return;
+  }
+  
+  const char* event_type = doc["eventType"];
+  const int event_value = doc["value"];
+
+  Serial.println("Type: " + String(event_type));
+  Serial.println("Value: " + String(event_value));
+
+  handleValidEvents(event_type, event_value);
+  return;
+}
